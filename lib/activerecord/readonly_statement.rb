d33ab99a1462bf8d7e8481ffbb55cecfaac3b29a
Takatoshi Ono <takatoshi.ono@gmail.com>
2018-04-25 23:19:04 +0900

    initial commit

---
+++ lib/activerecord/readonly_statement.rb
@@ -0,0 +1,7 @@
+require "activerecord/readonly_statement/version"
+
+module Activerecord
+  module ReadonlyStatement
+    # Your code goes here...
+  end
+end
---
+++ lib/activerecord/readonly_statement/version.rb
@@ -0,0 +1,5 @@
+module Activerecord
+  module ReadonlyStatement
+    VERSION = "0.1.0"
+  end
+end
